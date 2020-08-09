a58bb73424dffb76d416d23478a099bec6c09b26
minhthao <minhthao5648@gmail.com>
2020-08-09 21:49:51 +0700

    fix err on mobile

--- src/components/BannerIamge/BannerImage.js
+++ src/components/BannerIamge/BannerImage.js
@@ -10,12 +10,20 @@ export default function BannerImage({ img, before, mid, after }) {
   return (
     <div className="banner" style={{ backgroundImage: `url(${img})` }}>
       <div className="banner__over">
-        <div className="banner__content" data-aos="fade-left">
-          {before && <b className="banner__before">{before}</b>}
+        <div className="banner__content">
+          {before && (
+            <b className="banner__before" data-aos="fade-left">
+              {before}
+            </b>
+          )}
           {after ? null : <br />}
-          <span className="banner__mid">{mid}</span>
+          <span className="banner__mid" data-aos="fade-left">
+            {mid}
+          </span>
           <br />
-          <b className="banner__after">{after}</b>
+          <b className="banner__after" data-aos="fade-left">
+            {after}
+          </b>
           <br />
           {before && after ? (
             <button className="banner__btn" data-aos="fade-left">
