b0057b486d295924ea41244cab9af2a3419fd284
minhthao <minhthao5648@gmail.com>
2020-08-11 10:33:55 +0700

    add new image and auto play news

--- src/App.js
+++ src/App.js
@@ -7,9 +7,22 @@ import Video from "./components/Video/Video";
 import FixedButton from "./components/FixedButton/FiexdButton";
 import Footer from "./components/Footer/Footer";
 // img
-import ImageBannerCity from "./assets/images/city.jpg";
-import ImageBannerGreenHose from "./assets/images/greenhose.png";
-import ImageBannerDailyFarm from "./assets/images/milk.jpg";
+import ImageBannerCity from "./assets/images/homer.jpg";
+import ImageBannerGreenHose from "./assets/images/banner.jpg";
+import ImageBannerDailyFarm from "./assets/images/feedback.jpg";
+import ImageBannerNews1 from "./assets/images/bannernews1.jpg"
+import ImageBannerNews2 from "./assets/images/bannernews2.jpg"
+// import Swiper core and required components
+import SwiperCore, { Navigation, Pagination, A11y, Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
+
+// install Swiper components
+SwiperCore.use([Navigation, Pagination, A11y, Autoplay]);
 
 function App() {
   return (
@@ -22,12 +35,35 @@ function App() {
         mid="BUIDING"
         after="MANAGEMENT"
       />
-      <Banner
-        img={ImageBannerGreenHose}
-        before="FARM"
-        mid="BY LAPTOP"
-        after="MANAGEMENT"
-      />
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        autoplay={{ delay: 3000 }}
+      >
+        <SwiperSlide> <Banner
+          img={ImageBannerGreenHose}
+          before="FARM"
+          mid="BY LAPTOP"
+          after="MANAGEMENT"
+        /></SwiperSlide>
+        <SwiperSlide> <Banner
+          img={ImageBannerNews1}
+          before="FARM"
+          mid="BY LAPTOP"
+          after="MANAGEMENT"
+        /></SwiperSlide>
+        <SwiperSlide> <Banner
+          img={ImageBannerNews2}
+          before="FARM"
+          mid="BY LAPTOP"
+          after="MANAGEMENT"
+        /></SwiperSlide>
+
+      </Swiper>
+
       <Video />
       <Banner
         img={ImageBannerDailyFarm}
